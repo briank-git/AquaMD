7c157dff29fbaf12685711515fbd87bc69fdb44d
briankim <briankim2011@gmail.com>
2020-07-03 15:40:47 -0700

    Initialized aquamd module

---
+++ src/app/aquamd/aquamd.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AquamdRoutingModule } from './aquamd-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AquamdRoutingModule
+  ]
+})
+export class AquamdModule { }
